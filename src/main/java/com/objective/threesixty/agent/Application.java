7e9ebf896c2a519434d34379c1fe59e416ccb418
ocl-pchu <patrick.chu@objective.com>
2025-03-18 14:45:40 -0700

    Update Application.java

--- src/main/java/com/objective/threesixty/agent/Application.java
+++ src/main/java/com/objective/threesixty/agent/Application.java
@@ -4,7 +4,7 @@ package com.objective.threesixty.agent;
  * %%
  * 3Sixty Remote Agent Example
  * -
- * Copyright (C) 2024 Objective Corporation Limited.
+ * Copyright (C) 2025 Objective Corporation Limited.
  * -
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
